f385824a3bf37fd19e40be46dc78c4296492f74e
ALEXANDER-AQUI <94832320+ALEXANDER-AQUI@users.noreply.github.com>
2026-02-07 00:05:28 -0500

    Update stream.sh

--- stream.sh
+++ stream.sh
@@ -2,7 +2,7 @@
 # VERSIÓN OPTIMIZADA PARA 100 GB/MES (34+ DÍAS 24/7)
 YOUTUBE_KEY="${YOUTUBE_KEY:-your-youtube-code}"
 SERVER_PORT="${PORT:-8080}"
-VIDEO_DIR="/app"
+VIDEO_DIR="/app/videos"
 
 apt-get update && apt-get install -y ffmpeg nginx
 
@@ -52,7 +52,7 @@ stream_to_youtube() {
         echo "Streaming en 480p @ 15fps (optimizado 100GB/mes)..."
         
         # CONFIGURACIÓN OPTIMIZADA PARA 100 GB/MES
-        ffmpeg -f concat -safe 0 -stream_loop -1 \
+        ffmpeg -f concat -safe 0 \
             -re -i /tmp/playlist.txt \
             -c:v libx264 \
             -preset veryfast \
@@ -78,4 +78,3 @@ stream_to_youtube() {
 }
 
 stream_to_youtube
-
